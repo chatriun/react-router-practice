57fed336e0d5b0c93c40f6f5e3bba1a30e7a2c89
salysaly <anyawee.sr@gmail.com>
2024-06-17 23:06:57 +0700

    style(component): filmItem

--- frontend/src/components/FilmItem.js
+++ frontend/src/components/FilmItem.js
@@ -1,6 +1,9 @@
 import { Box, Button, Typography } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2/Grid2";
 import { useNavigate, useSubmit } from "react-router-dom";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import EditNoteIcon from "@mui/icons-material/EditNote";
+import CloseIcon from "@mui/icons-material/Close";
 
 const FilmItem = ({ film }) => {
   const navigate = useNavigate();
@@ -14,83 +17,127 @@ const FilmItem = ({ film }) => {
   };
 
   return (
-    <Box width="100%" p={2}>
+    <Box p={2}>
       <Grid
         container
         display="flex"
-        alignItems="center"
         spacing={2}
         m={0}
-        p={1}
+        p={2}
         border={1}
         borderColor="black"
         borderRadius={4}
         backgroundColor="white"
       >
-        <Grid item sx={12} md={4}>
-          <Box
-            display="flex"
-            flexDirection="column"
-            justifyContent="space-between"
+        <Grid item xs={12} md={2} display="flex" flexDirection="column" gap={2}>
+          <Typography
+            fontSize={{ xs: 0, md: 150, xl: 220 }}
+            lineHeight={{ md: 0.7 }}
+            mb="auto"
+            sx={{ color: "#8A2BE2" }}
           >
-            <Typography
-              variant="h3"
-              fontWeight="bolder"
-              width="100%"
-              textOverflow="ellipsis"
-              sx={{ color: "black" }}
-              lineHeight="42px"
-              overflow="hidden"
-              mr="auto"
-              mb={1}
+            ✸
+          </Typography>
+          <Typography
+            variant="h3"
+            fontWeight="bolder"
+            width="100%"
+            textOverflow="ellipsis"
+            lineHeight="42px"
+            overflow="hidden"
+            sx={{ color: "black", wordBreak: "keep-all" }}
+          >
+            {film.title}
+          </Typography>
+        </Grid>
+        <Grid
+          item
+          sx={12}
+          md={3}
+          display="flex"
+          flexDirection="column"
+          justifyContent="space-between"
+        >
+          <Typography
+            variant="body2"
+            fontWeight="bold"
+            mb={5}
+            sx={{ color: "#8A2BE2" }}
+          >
+            {film.date}
+          </Typography>
+          <Typography variant="subtitle2" mb={4} sx={{ color: "black" }}>
+            {film.description}
+          </Typography>
+          <Box item display="flex" gap={2} mt="auto" mb={1}>
+            <Button
+              variant="outlined"
+              onClick={() => navigate("..", { relative: "path" })}
+              sx={{
+                flexWrap: "wrap",
+                wordBreak: "break-all",
+                borderRadius: 20,
+                borderColor: "#8A2BE2",
+                alignItems: "center",
+                gap: "2px",
+              }}
             >
-              {film.title}
-            </Typography>
-            <Typography
-              variant="body2"
-              fontWeight="bold"
-              mb={5}
-              sx={{ color: "#8A2BE2" }}
+              <ArrowBackIcon fontSize="small" sx={{ color: "#8A2BE2" }} />
+              <Typography
+                variant="button"
+                lineHeight={1}
+                sx={{ color: "#8A2BE2" }}
+              >
+                back
+              </Typography>
+            </Button>
+            <Button
+              variant="contained"
+              size="small"
+              onClick={() => navigate("edit")}
+              sx={{
+                flexWrap: "wrap",
+                wordBreak: "break-all",
+                borderRadius: 20,
+                backgroundColor: "#8A2BE2",
+                alignItems: "center",
+                gap: "2px",
+              }}
             >
-              {film.date}
-            </Typography>
-            <Typography variant="subtitle2" mb={4} sx={{ color: "black" }}>
-              {film.description}
-            </Typography>
-            <Box item display="flex" gap={2} mt="auto" mb={1}>
-              <Button
-                variant="outlined"
-                onClick={() => navigate("..", { relative: "path" })}
-                sx={{ borderRadius: 20, borderColor: "#8A2BE2" }}
+              <EditNoteIcon fontSize="small" />
+              <Typography
+                variant="button"
+                lineHeight={1}
+                sx={{ color: "white" }}
               >
-                <Typography variant="button" sx={{ color: "#8A2BE2" }}>
-                  ↩︎ back
-                </Typography>
-              </Button>
-              <Button
-                variant="contained"
-                size="small"
-                onClick={() => navigate("edit")}
-                sx={{ borderRadius: 20, backgroundColor: "#8A2BE2" }}
+                edit
+              </Typography>
+            </Button>
+            <Button
+              variant="contained"
+              size="small"
+              sx={{
+                flexWrap: "wrap",
+                wordBreak: "break-all",
+                borderRadius: 20,
+                backgroundColor: "#8A2BE2",
+                alignItems: "center",
+                gap: "2px",
+              }}
+              onClick={handleDelete}
+            >
+              <CloseIcon fontSize="small" />
+              <Typography
+                variant="button"
+                lineHeight={1}
+                sx={{ color: "inherit" }}
               >
-                <Typography variant="button" sx={{ color: "white" }}>
-                  ✎ edit
-                </Typography>
-              </Button>
-              <Button
-                variant="contained"
-                size="small"
-                sx={{ borderRadius: 20, backgroundColor: "#8A2BE2" }}
-                onClick={handleDelete}
-              >
-                <Typography variant="button" sx={{ color: "inherit" }}>
-                  ✖︎ delete
-                </Typography>
-              </Button>
-            </Box>
+                delete
+              </Typography>
+            </Button>
           </Box>
         </Grid>
-        <Grid item xs={12} md={8}>
+        <Grid item xs={12} md={7}>
           <img
             src={film.url}
             alt={film.title}
